803992decc19692dc5a25a172c0e46e38f9b9bb9
Withney Guilherme <wguilhermecriativo@gmail.com>
2024-11-10 12:32:46 -0300

    Initial Commit

---
+++ QuickLauncher/QuickLauncherApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickLauncherApp.swift
+//  QuickLauncher
+//
+//  Created by Withney Guilherme on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct QuickLauncherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuickLauncherTests/QuickLauncherTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuickLauncherTests.swift
+//  QuickLauncherTests
+//
+//  Created by Withney Guilherme on 10/11/24.
+//
+
+import Testing
+@testable import QuickLauncher
+
+struct QuickLauncherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
